a1a2c5cacb067867ba33f01d89907053c50de68d
Mister2SI <jonahlotegeluaki@gmail.com>
2026-09-04 13:04:46 -0500

    Complete Counter.java assignment

--- InClassNotes/src/main/java/week1/Counter.java
+++ InClassNotes/src/main/java/week1/Counter.java
@@ -18,4 +18,42 @@ public class Counter {
     // 8. Add a reset method
 
     //9. Add a fixed-size history array that records the last 5 states of the counter. All logic must happen inside of the object and be hidden from the user.
+    
+    
+    private int count;
+    private int[] hist = new int[5];
+
+    private void appendHist(int n) {
+        // Shift all elements right, discarding [4]
+        for (int i=4;i>0;i--) {
+            hist[i] = hist[i-1];
+        }
+
+        hist[0] = n;
+    }
+
+
+    public Counter() {
+        count = 0;
+    }
+    public Counter(int i) {
+        count = i;
+    }
+
+    public int getCount() { return count; }
+    public void increment() {
+        count += 1;
+        appendHist(count);
+    }
+    public void increment(int c) {
+        count += c;
+        appendHist(count);
+    }
+
+    public void reset() {
+        count = 0;
+        appendHist(count);
+    }
+
+
 }
