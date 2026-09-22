96d6fd04c0f1a4cf254b7749cf45e0fd904907f3
JonahLotegeluaki <jonahloteg@gmail.com>
2026-09-22 10:21:46 -0500

    Add student tests

--- HospitalSimulatorHandout/src/test/java/triage_efficiency/SortStudentTests.java
+++ HospitalSimulatorHandout/src/test/java/triage_efficiency/SortStudentTests.java
@@ -6,15 +6,48 @@ import patient_intake.*;
 
 public class SortStudentTests {
     
+    EfficiencyTester alg = new EfficiencyTester();
+    Patient[] arr = {
+        new Patient("P002", "b", "b", 2, "z", 2, "z", "z", 2, "z"),
+        new Patient("P001", "a", "a", 1, "z", 1, "z", "z", 1, "z"),
+        new Patient("P003", "c", "c", 3, "z", 3, "z", "z", 3, "z"),
+        new Patient("P006", null, null, 0, null, 0, null, null, 0, null),
+        new Patient("P004", null, null, 0, null, 0, null, null, 0, null),
+        new Patient("P011", null, null, 0, null, 0, null, null, 0, null),
+    };
+
     // Tests that the sorting function works correctly.
     // Necessary for binary search.
     @Test 
     void testArraySorting() {
-        Patient[] arr = new Patient[2];
-        arr[1] = new Patient("P001", "a", "a", 1, "z", 1, "z", "z", 1, "z");
-        arr[0] = new Patient("P002", "b", "b", 2, "z", 2, "z", "z", 2, "z");
         assertEquals("P001", arr[1].getPatientID());
-        Patient[] arr2 = PatientRegistry.sortByID(arr);
+        Patient[] arr2 = Main.sortByPatientId(arr);
         assertEquals("P001", arr2[0].getPatientID());
+    }
+
+    // Test that linear search works correctly.
+    // Necessary for credit.
+    @Test 
+    void testLinearSearch() {
+        assertEquals(2, alg.linearSearch(arr, "P002").getAge());
+        assertEquals(null, alg.linearSearch(arr, "P013"));
+    }
+
+    // Test that binary search works correctly.
+    // Required for credit.
+    @Test 
+    void testBinarySearch() {
+        Patient[] arr2 = Main.sortByPatientId(arr);
+        assertEquals("b", alg.binarySearch(arr2, "P002").getFirstName());
+        assertEquals(null, alg.binarySearch(arr2, "P199"));
+    }
+
+    // Test that exponential search works correctly.
+    // Required for optional credit.
+    @Test 
+    void testExponentialSearch() {
+        Patient[] arr2 = Main.sortByPatientId(arr);
+        assertEquals(3, alg.logNSearch(arr2, "P003").getAge());
+        assertEquals(null, alg.logNSearch(arr2, "P023"));
     }
 }
